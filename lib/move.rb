0182c778c11071199efb73893d403838d22bea2c
Anne Edmondson <edmondson.anne@gmail.com>
2018-07-08 17:43:17 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+<<<<<<< HEAD
 def input_to_index(input)
   converted_input = input.to_i
   converted_input - 1
@@ -15,3 +16,20 @@ def move(board, index, value = "X")
   board[index] = value
 end
 
+=======
+def input_to_index(user_input)
+  converted_input = user_input.to_i
+  converted_input - 1
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " ",]
+ 
+
+def move(board, index, value = "X")
+ 
+ index = input_to_index(user_input)
+ 
+ board[index] = value
+end
+  
+>>>>>>> 779eb7f4eca8717db5f53b696635b428751d0459
